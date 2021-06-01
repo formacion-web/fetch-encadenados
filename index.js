a69ff6f4efbf4588426c2d5e7d444f501369a421
prueba <you@mail.com>
2021-06-01 14:45:16 +0200

    init

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Asocia la función que cargará el listado de países al evento de carga de la página.
 let selectCountries = document.querySelector('.country');
-let selectNeighbors = document.querySelector('.neighbors');
+let selectNeighbors = document.querySelector('.neighbor');
 let loadCountries = () =>{
     //Traer los países -- fetch
     //Los cargo en el elemento html
@@ -24,23 +24,26 @@ let loadCountries = () =>{
 let loadNeighbors = () =>{
 
      let parametros = {
-    //     method: 'get',
+         method: 'get',
          mode: 'no-cors',
-    //     cache: 'no-cache',
-    //     credentials: 'include',
-    //     headers:{
-    //         'Access-Control-Allow-Origin': 'http://localhost:3000',
-    //         'Access-Control-Allow-Credentials':true,
-    //         'Content-Type':'text/json'
-    //     }
+         cache: 'no-cache',
+         credentials: 'include',
+         headers:{
+             'Access-Control-Allow-Origin': 'https://fetch-encadenados.netlify.app',
+             'Access-Control-Allow-Credentials':true,
+             'Content-Type':'text/json'
+         }
     }
     let countryCode = selectCountries.options[selectCountries.selectedIndex].value;
-    let neighbors = fetch('https://api.geodatasource.com/neighbouring-countries?key=7PUKVKJJCBNQHZQOQO3ZJVBCVHZJHTEP&country_code='+countryCode);
+    let neighbors = fetch('https://api.geodatasource.com/neighbouring-countries?key=7PUKVKJJCBNQHZQOQO3ZJVBCVHZJHTEP&country_code='+countryCode,{headers:{'Access-Control-Allow-Origin':"*"}});//+countryCode,parametros);
     neighbors.then(response =>response.json())
-             .then(data => data.forEach(element =>{
+             .then(data =>{
+                console.log(data) ;
+                data.forEach(element =>{
+                    console.log(element) ;
                 let option = new Option(element.country_name,element.country_code);
                 selectNeighbors.add(option);
-             }))
+             })})
              .catch(error => console.log(error));
 }
 
